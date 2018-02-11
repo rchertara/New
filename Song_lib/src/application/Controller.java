2e264a05681486261c530e98a04d1cd10b51d2f4
Rahil <Chertara>
2018-02-11 13:24:22 -0800

    text file saved
    hate

---
+++ Song_lib/src/application/Controller.java
@@ -0,0 +1,257 @@
+package application;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Optional;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class Controller{
+	  
+		
+	//Rahil Chertara
+	
+	//Buttons
+	@FXML Button addButton;
+	@FXML Button editButton;
+	@FXML Button delButton;
+	@FXML Button saveButton;
+	//TextField	1-4
+	@FXML TextField nameField;
+	@FXML TextField artistField;
+	@FXML TextField albumField;
+	@FXML TextField yearField;
+	//TextField 5-8
+	@FXML TextField nameFieldedit;
+	@FXML TextField artistFieldedit;
+	@FXML TextField albumFieldedit;
+	@FXML TextField yearFieldedit;
+	//Labels
+	@FXML Label nameLabel;
+	@FXML Label artistLabel;
+	@FXML Label albumLabel;
+	@FXML Label yearLabel;
+	
+	//ListView
+	@FXML public ListView<SongNode> listView;
+	
+	SongLibrary library = new SongLibrary();
+
+	
+	ObservableList<String> stringList= FXCollections.observableArrayList();
+	
+	
+	
+	
+	 //@SuppressWarnings("unchecked")
+	@FXML
+	    public void initialize() {
+			 library.parse(); //always scan in txt file
+		     library.sortAscending(); //always sort that textfile
+			
+		     
+		     // library.Add(new SongNode("Rahil","Hate"));
+			// library.Add(new SongNode("Rahil","Hate"));
+		       
+		        
+			 
+			 
+			 
+			 
+		  listView.setItems(library.getsongList());
+		  listView.getSelectionModel();
+		  
+		 if (library.getsongList().size() > 0) {
+	       listView.getSelectionModel().select(0);
+	       if(listView.getSelectionModel().getSelectedIndex()==0) {
+				SongNode node = library.songList.get(listView.getSelectionModel().getSelectedIndex());
+				   nameLabel.setText(node.SongName);
+				   artistLabel.setText(node.ArtistName);
+				   albumLabel.setText(node.Album);
+				   yearLabel.setText(node.Year);
+			}
+	       
+	      }
+	     
+	     // int index = listView.getSelectionModel().getSelectedIndex();
+		  //listView.getSelectionModel().select(index);
+		    
+		//setting edit stuff to non-visible
+			nameFieldedit.setDisable(true);
+			artistFieldedit.setDisable(true);
+			albumFieldedit.setDisable(true);
+			yearFieldedit.setDisable(true);
+	}//initialize method
+	
+	
+	public void details() {
+		
+		if(library.songList.isEmpty()) {
+			   //Song item = songs.songList.get(listView.getSelectionModel().getSelectedIndex());
+			   nameLabel.setText("");
+			   artistLabel.setText("");
+			   albumLabel.setText("");
+			   yearLabel.setText(""); 
+			   
+			   
+		}
+			   
+		
+		else {
+			
+			     
+				
+				   SongNode node = library.songList.get(listView.getSelectionModel().getSelectedIndex());
+				 
+				  
+				   nameLabel.setText(node.SongName);
+				   artistLabel.setText(node.ArtistName);
+				   albumLabel.setText(node.Album);
+				   yearLabel.setText(node.Year);
+				  
+			 
+		}
+			   
+		}
+	
+	
+	
+	
+	public void saveClicked() {
+		
+		library.toFile();
+		return;
+		
+		
+	}
+
+	
+	public void listSelect() {
+	listView.getSelectionModel().getSelectedItem();
+   
+	details(); //show details
+	
+	//when something is selected, do the following:
+	//setting edit stuff to non-visible
+			nameFieldedit.setDisable(false);
+			artistFieldedit.setDisable(false);
+			albumFieldedit.setDisable(false);
+			yearFieldedit.setDisable(false);
+			
+			//
+	
+	}
+	
+
+	public void addClicked() {
+	
+		if(nameField.getText().equals("")) {
+			Alert alert_name = new Alert(Alert.AlertType.WARNING, "Song name is empty.", ButtonType.OK);
+			alert_name.showAndWait();
+		}
+		else if(artistField.getText().equals("")) {
+			Alert alert_artist = new Alert(Alert.AlertType.WARNING, "Artist name is empty.", ButtonType.OK);
+			alert_artist.showAndWait();
+		}
+		else {
+			Alert alert_confirm = new Alert(Alert.AlertType.CONFIRMATION);
+			alert_confirm.setTitle("Confirmation of Addition");
+			alert_confirm.setHeaderText("Confirm the following:");
+			Optional<ButtonType> result = alert_confirm.showAndWait();
+			if (result.get() == ButtonType.OK){
+			    // ... user chose OK
+				SongNode newNode= new SongNode(nameField.getText(),artistField.getText(), albumField.getText(),yearField.getText());
+				
+		        library.Add(newNode);
+		        //has sort in it 
+		        int index=library.songList.indexOf(newNode);
+		        
+		           listView.getSelectionModel().select(index);//as soon as you add must select it 
+		          
+		           //show details as soon as you add,and its auto selected
+		        	   nameLabel.setText(newNode.SongName);
+				   artistLabel.setText(newNode.ArtistName);
+				   albumLabel.setText(newNode.Album);
+				   yearLabel.setText(newNode.Year);
+		       
+				   listView.setItems(library.getsongList());
+			} 
+			
+			else {
+			    // ... user chose CANCEL or closed the dialog
+				
+				}
+     
+	     }
+	
+	}
+	
+	public void editClicked() {
+		
+
+		if(nameFieldedit.getText().equals("")) {
+			Alert alert_name = new Alert(Alert.AlertType.WARNING, "Song name is empty.", ButtonType.OK);
+			alert_name.showAndWait();
+		}
+		else if(artistFieldedit.getText().equals("")) {
+			Alert alert_artist = new Alert(Alert.AlertType.WARNING, "Artist name is empty.", ButtonType.OK);
+			alert_artist.showAndWait();
+		}
+		else {
+			Alert alert_confirm = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to make these changes?");
+			alert_confirm.setTitle("Confirmation of Edit");
+			alert_confirm.setHeaderText("Confirm the following:");
+			Optional<ButtonType> result = alert_confirm.showAndWait();
+			if (result.get() == ButtonType.OK){
+				
+				if(listView.getSelectionModel().isEmpty()) {
+					Alert alert = new Alert(Alert.AlertType.WARNING, "Cannot Edit From Empty List.", ButtonType.OK);
+					alert.showAndWait();
+					return;
+				}
+			    // ... user chose OK
+				SongNode song = listView.getSelectionModel().getSelectedItem();	
+				library.Edit(song.SongName, song.ArtistName, nameFieldedit.getText(), artistFieldedit.getText(), albumFieldedit.getText(), yearFieldedit.getText());
+				listView.setItems(library.getsongList());
+			} else {
+			    // ... user chose CANCEL or closed the dialog
+				
+			}
+	        
+		}
+	}
+	
+	public void delClicked() {
+		Alert alert_confirm = new Alert(Alert.AlertType.CONFIRMATION);
+		alert_confirm.setTitle("Confirmation of Deletion");
+		alert_confirm.setHeaderText("Confirm the following:");
+		Optional<ButtonType> result = alert_confirm.showAndWait();
+		if (result.get() == ButtonType.OK){
+		    // ... user chose OK
+			if(listView.getSelectionModel().isEmpty()) {
+				Alert alert = new Alert(Alert.AlertType.WARNING, "Cannot Delete From Empty List.", ButtonType.OK);
+				alert.showAndWait();
+				return;
+			}
+			
+			
+			SongNode song = listView.getSelectionModel().getSelectedItem();	
+			int index=library.songList.indexOf(song); //find the index of this song in AL
+			library.Delete(song.SongName, song.ArtistName);
+			listView.getSelectionModel().select(index);
+			listView.setItems(library.getsongList());
+		 }
+		
+		else {
+		    // ... user chose CANCEL or closed the dialog
+			
+		}
+		
+        //alert_confirm.showAndWait();
+	}
+
+
+}
---
+++ Song_lib/src/application/SongLibrary.java
@@ -0,0 +1,221 @@
+package application;
+import java.io.BufferedReader;
+
+
+import javafx.scene.control.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Scanner;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class SongLibrary {
+	
+	   private final static String fileName ="hate.txt";//Name of the song data!
+		
+	//Rahil Chertara 
+	
+	//public songList<SongNode> arrayList;
+	ObservableList<SongNode> songList;
+	
+	
+ 
+	public SongLibrary() {
+		//this.songList=FXCollections.observablesongList();
+		 // this.songList= new songList<SongNode>();
+		  this.songList = FXCollections.observableArrayList();
+		 
+		
+	}
+	public ObservableList<SongNode> getsongList() {  //still not sure if i need this        
+		    return this.songList;     
+    }    
+	
+	public ObservableList<SongNode> sortAscending() {         
+		    FXCollections.sort(this.songList);         
+		    return this.songList;     
+	}       
+	 public void print() {
+	    	
+	      	for (int i = 0; i < this.songList.size(); i++) {
+	            SongNode val =this.songList.get(i);
+	            
+	            System.out.println(val.SongName+"-->"+val.ArtistName);
+	        }
+	     	 
+	    }
+	public void parse(){
+		String text="";
+		BufferedReader buffer = null;
+		try {
+			buffer = new BufferedReader(new FileReader("src/application/"+fileName));
+		}catch(FileNotFoundException e){
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	
+	      
+			try {
+				while((text=buffer.readLine())!=null) {
+				String textArray[]= new String[4];
+				
+				String temp[]= text.split(",");
+				
+				for(int i=0;i<temp.length;i++) {
+					textArray[i]=temp[i];
+				}
+					
+				SongNode newNode=new SongNode(textArray[0],textArray[1],textArray[2],textArray[3]);	
+				
+			//MUST MAKE A CATCH HERE FOR if songs are dupliate 
+				this.songList.add(newNode);
+			
+					
+					
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+  
+			
+	}
+		public void toFile() {
+		
+		Writer writer = null;
+				try {
+					writer = new FileWriter("src/application/"+fileName);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	        	//
+	        	for(int i=0;i<songList.size();i++) {
+	        		SongNode node=songList.get(i);
+	        		
+	        		String line= node.toText();
+	        		try {
+						writer.write(line);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+	        		
+	        	}
+	        		
+	        		
+	        		try {
+					writer.close();
+					return;
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					return;
+				}
+	        
+	      
+		}
+	
+	
+	
+	
+	public void Add(SongNode newSong) {
+		
+		if(!songList.contains(newSong)){
+		
+			
+			songList.add(newSong);
+		    
+			FXCollections.sort(songList);// for each add you must put in correct spot
+		    //could you binary search to find correct spot but much easier
+			
+			return;
+		}
+		
+		else {
+			Alert alert = new Alert(Alert.AlertType.WARNING, "Song already in list.", ButtonType.OK);
+			alert.showAndWait();
+			 return;
+		}
+	
+	
+	}
+	
+	
+	public void Delete(String SongName, String ArtistName) {
+		
+		for (int i = 0; i < songList.size(); i++) {
+            SongNode node =songList.get(i);
+           if(node.getSongName()==SongName && node.getArtistName()==ArtistName) {
+        	   songList.remove(i);
+        	   return;
+           }
+           else {
+        	   continue;
+           }
+            
+        }
+		
+		FXCollections.sort(songList);
+		return;
+		
+		
+	}
+	
+	public void Edit(String currSong,String currArtist, String newName, String newArtist, String newAlb, String newYear) {
+		
+ 
+		SongNode target= new SongNode(newName,newArtist,newAlb,newYear);
+	
+		int targetIndex= songList.indexOf(target);
+      
+			if(targetIndex==-1) {
+			
+				for (int i = 0; i < songList.size(); i++) {
+			    SongNode node =songList.get(i);
+				if(node.getSongName()==currSong && node.getArtistName()==currArtist) {
+       
+					songList.remove(i);
+					songList.add(target);
+					
+//					node.setSongName(newName);
+//					node.setArtistName(newArtist);
+//					node.setAlbum(newAlb);
+//					node.setYear(newYear);
+        	   
+					
+					
+					
+					FXCollections.sort(songList);
+					return;
+				}
+           
+				else {
+					continue;
+				}
+            
+				}
+		
+		 }
+			
+			else {
+				Alert alert = new Alert(Alert.AlertType.WARNING, "Song already in list. Edit cannot be made.", ButtonType.OK);
+				alert.showAndWait();
+				return;
+			}
+	
+	
+	}
+	
+	
+	
+}
+	
+	
+
+
